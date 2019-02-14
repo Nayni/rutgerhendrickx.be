47c3fef07b631c72b4a720a73275a7ba1d133c9a
nayni <rutgerhendrickx@gmail.com>
2019-02-14 23:25:08 +0100

    Add explanation for material-ui/styles SSR & Client render

--- .storybook/config.js
+++ .storybook/config.js
@@ -6,6 +6,9 @@ import {
   install
 } from "@material-ui/styles";
 
+// Install the new styling solution of material-ui/styles.
+// This step is temporary until the core library ships with the new solution by default.
+// We have to do this before we import any material-ui components to ensure the engine is swapped and styles are applied correctly.
 install();
 
 import { configure, addDecorator } from "@storybook/react";
@@ -16,9 +19,9 @@ import { createTheme } from "../src/createTheme";
 // automatically import all files ending in *.stories.js
 const req = require.context("../src", true, /.stories.[jt]sx?$/);
 
-function loadStories() {
+const loadStories = () => {
   req.keys().forEach(filename => req(filename));
-}
+};
 
 // Gatsby's Link overrides:
 // Gatsby defines a global called ___loader to prevent its method calls from creating console errors you override it here
@@ -33,6 +36,8 @@ window.___navigate = pathname => {
   action("NavigateTo:")(pathname);
 };
 
+// All components are built using material-ui.
+// Wrap every storyin the correct providers and ensure we have a consistent baseline to start from.
 const theme = createTheme();
 const generateClassName = createGenerateClassName({
   dangerouslyUseGlobalCSS: false,
--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,6 +8,9 @@ const {
   install
 } = require("@material-ui/styles");
 
+// Install the new styling solution of material-ui/styles.
+// This step is temporary until the core library ships with the new solution by default.
+// We have to do this before we import any material-ui components to ensure the engine is swapped and styles are applied correctly.
 install();
 
 const CssBaseline = require("@material-ui/core/CssBaseline").default;
@@ -19,6 +22,9 @@ const onInitialClientRender = () => {
   ssStyles && ssStyles.parentNode.removeChild(ssStyles);
 };
 
+// We removed the server-side injected CSS.
+// So we have to re-render on the client using the same settings as the server.
+// We wrap the root elemement again in the correct providers.
 const wrapRootElement = ({ element }, options) => {
   const generateClassName = createGenerateClassName({
     dangerouslyUseGlobalCSS: false,
@@ -38,6 +44,9 @@ const wrapRootElement = ({ element }, options) => {
   );
 };
 
+// Wrap the hydrate function to aid in HMR in development mode.
+// This still doesn't fix the styles HMR completely though
+// See: https://github.com/gatsbyjs/gatsby/issues/8237
 const replaceHydrateFunction = () => {
   return (element, container, callback) => {
     ReactDOM.render(element, container, callback);
